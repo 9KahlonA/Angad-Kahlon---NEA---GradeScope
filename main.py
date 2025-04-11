fa49fdf3f42de4fb2e28f7c275dddee48de5815a
kahlon-ask <kahlon.ask@gmail.com>
2025-04-11 13:48:04 +0100

    Structure
    Changes to Flask structure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from website.templates import create_app  # Adjust the import path to match the module structure
+from website import create_app  # Adjust the import path to match the module structure
 app = create_app()
 
 if __name__ == '__main__':  # Run the app in debug mode for development without having to reload every time.
