a0df5dacf89f88ecf7079062ba21e4bbe928fd57
kahlon-ask <kahlon.ask@gmail.com>
2025-04-11 13:43:57 +0100

    Flask bilding

--- website/templates/__init__.py
+++ website/templates/__init__.py
@@ -8,8 +8,8 @@ def create_app():
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, name="VIEWS", url_prefix='/')
+    app.register_blueprint(auth, name="AUTH", url_prefix='/')
 
     return app
 
--- website/templates/auth.py
+++ website/templates/auth.py
@@ -2,3 +2,10 @@ from flask import Blueprint
 
 auth = Blueprint('views', __name__)
 
+@auth.route('/login')
+def login():
+    return "<h1>Login</h1>"
+
+@auth.route('/logout')
+def logout():
+    return "<h1>Logout</h1>"
