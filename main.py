e8905c55c2969ee9aecc9089f76c79aca2c13bee
kahlon-ask <kahlon.ask@gmail.com>
2025-04-11 12:34:07 +0100

    Flask Framework
    Building The Flask Framework which will create a web app that can be hosted

---
+++ main.py
@@ -0,0 +1,5 @@
+from website import create_app  # Adjust the import path to match the module structure
+app = create_app()
+
+if __name__ == '__main__':  # Run the app in debug mode for development without having to reload every time.
+    app.run(debug=False)  # Ensure debug mode is off in production
---
+++ website/templates/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'GradesScopeDEV' # Change this to a random secret key in production
+
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    return app
+
+# This file is reponsible for creating the instance for the Flask app. DO NOT MODIFY THIS FILE.
